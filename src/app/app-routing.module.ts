93615e431536f0169c230c184dc0cc4c5f61f023
bara0131 <bara0131@algonquinlive.com>
2023-02-28 12:30:03 -0500

    sample route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [{ path: 'home', component: AppComponent }];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
