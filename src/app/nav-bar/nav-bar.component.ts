093942ec307fe7e4badd3fa5b52a35c1fcb39c10
bara0131 <bara0131@algonquinlive.com>
2023-03-03 23:14:27 -0500

    nav bar scroll

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('navbarSmallScreenShowElement', { static: true }) myDiv:
+    | ElementRef
+    | undefined;
 
+  ngAfterViewInit(): void {}
+
+  navbarSmallScreenShow: boolean = false;
+  iconNames = ['menu', 'keyboard_arrow_down'];
+  currentIconIndex = 0;
+  onClickIcon() {
+    this.currentIconIndex = (this.currentIconIndex + 1) % this.iconNames.length;
+    //this.myDiv?.nativeElement.classList.add('fade-out-top');
+    this.navbarSmallScreenShow
+      ? this.myDiv?.nativeElement.classList.add('fade-out-top')
+      : this.myDiv?.nativeElement.classList.remove('fade-out-top');
+    console.log(this.myDiv);
+
+    this.navbarSmallScreenShow = !this.navbarSmallScreenShow;
+  }
 }
