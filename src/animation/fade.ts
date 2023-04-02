cf808a2e60f0b79dbbe286c1b40f1a453add7109
Parham Barati <bara0131@algonquinlive.com>
2023-04-02 00:24:27 -0400

    reduce fade time add z index

--- src/animation/fade.ts
+++ src/animation/fade.ts
@@ -9,7 +9,7 @@ import {
 export let fade = trigger('fade', [
   transition(':enter', [
     style({ opacity: 0, transform: 'translateY(-50px)' }),
-    animate(600),
+    animate(300),
   ]),
   transition(':leave', [
     animate(100, style({ transform: 'translateX(100%)' })),
